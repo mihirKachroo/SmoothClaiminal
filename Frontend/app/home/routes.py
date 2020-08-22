4d6f31e895e5d7e539ec0f9a5cf2de3e76146d62
mihirKachroo <mkachroo29@gmail.com>
2020-08-22 15:22:43 -0400

    Flask Integration Changed

--- Frontend/app/home/routes.py
+++ Frontend/app/home/routes.py
@@ -5,7 +5,7 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from app.home import blueprint
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
 from flask_login import login_required, current_user
 from app import login_manager
 from jinja2 import TemplateNotFound
@@ -16,19 +16,30 @@ def index():
 
     return render_template('index.html')
 
+
 @blueprint.route('/<template>')
 @login_required
-def route_template(template):
+def sdss(template):
 
-    try:
-
-        if not template.endswith( '.html' ):
-            template += '.html'
-
-        return render_template( template )
-
-    except TemplateNotFound:
+    if TemplateNotFound:
         return render_template('page-404.html'), 404
     
-    except:
+    else:
         return render_template('page-500.html'), 500
+
+@blueprint.route('/auth-signup.html')
+@login_required
+def fd():
+    return render_template('auth-signup.html')
+
+@blueprint.route('/auth-signin.html')
+@login_required
+def route_template():
+    return render_template('auth-signin.html')
+
+@blueprint.route('/upload.html',methods = ['POST', 'GET'])
+def upload():
+    message = ''
+    if request.method == 'POST':
+        message='works'
+    return render_template('upload.html', message=message)
