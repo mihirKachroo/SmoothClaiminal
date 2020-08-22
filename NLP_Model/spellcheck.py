3e4b42fd0339e5f170c58603f84a778cc696cfda
Your Name <markliu202@gmail.com>
2020-08-22 16:56:15 -0400

    Spellchecker replace all incorrect words with correct ones

--- NLP_Model/spellcheck.py
+++ NLP_Model/spellcheck.py
@@ -1,6 +1,12 @@
 ########### Python 3.2 #############
 import http.client, urllib.request, urllib.parse, urllib.error, base64
 
+# import json to print the data in a neater way
+import json
+
+import operator
+
+
 example_text = "hellooo this iss atest to ssee howgood thes spelll chcker is"
 
 headers = {
@@ -23,9 +29,50 @@ try:
     conn.request("POST", "/bing/v7.0/spellcheck/?%s" % params, "{body}", headers)
     response = conn.getresponse()
     data = response.read()
-    print(data)
+    # TYPE IS: bytes
+    # print(type(data))
+    # convert to string
+    data = data.decode('utf-8')
+    # TYPE IS NOW: str
+    # print(type(data))
+    # convert to json (dictionary)
+    data = json.loads(data)
+    # TYPE IS NOW: dict
+    # print(type(data))
+
+    # this is to print the data in a neater way IF YOU WANT:
+    # json_object = json.loads(data)
+    # json_formatted_str = json.dumps(json_object, indent=2)
+    # print(json_formatted_str)
+
+    # Replace incorrect words with correct words
+    text = example_text
+    data = data
+    shifting = 0
+    correct = text
+    for ft in data['flaggedTokens']:
+        offset = ft['offset']
+        suggestions = ft['suggestions']
+        token = ft['token']
+
+        # find the best suggestion
+        suggestions.sort(key=operator.itemgetter('score'), reverse=True)
+        substitute = suggestions[0]['suggestion']
+
+        # replace the token by the suggestion
+        before = correct[:offset + shifting]
+        after = correct[offset + shifting + len(token):]
+        correct = before + substitute + after
+        shifting += len(substitute) - len(token)
+
+    print("ORIGINAL:")
+    print(example_text)
+    print("\n")
+    print("CORRECTED:")
+    print(correct)
+
+
     conn.close()
 except Exception as e:
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
-
-####################################
+    # print("[Errno {0}] {1}".format(e.errno, e.strerror))
+    print(e)
